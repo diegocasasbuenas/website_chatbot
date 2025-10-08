75cae0415d00fd73721892bde5fb537ef517dd67
Juan León <juans.leonv@icloud.com>
2025-10-07 19:21:39 -0500

    feat: Fixing some bugs

--- src/app/components/ui/molecules/HeaderMolecule.tsx
+++ src/app/components/ui/molecules/HeaderMolecule.tsx
@@ -1,18 +1,18 @@
-import { Bars3Icon } from "@heroicons/react/16/solid";
 import Image from "next/image";
 import SocialMediaMolecule from "./SocialMediaMolecule";
+import Link from "next/link";
 
 export default function HeaderMolecule() {
   return (
     <header className="flex justify-between items-center">
-      <a href="/">
+      <Link href="/">
         <Image
           src="/images/Logo.svg"
           alt="Diego Casasbuenas Logo"
           width={69}
           height={36}
         />
-      </a>
+      </Link>
       <SocialMediaMolecule />
       {/* <Bars3Icon className="size-10 cursor-pointer" /> */}
     </header>
