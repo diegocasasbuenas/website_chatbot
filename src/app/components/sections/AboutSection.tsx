566f73198b6b14c4f011664f710b2e6835728060
Juan León <juans.leonv@icloud.com>
2025-10-13 21:37:03 -0500

    fix: Image portrair fixed

--- src/app/components/sections/AboutSection.tsx
+++ src/app/components/sections/AboutSection.tsx
@@ -24,7 +24,6 @@ export default function AboutSection() {
         alt="Profile Picture"
         width={450}
         height={350}
-        className="w-1/2 max-w-2xl"
       />
       <AccordionMolecule items={accordionInfo} />
     </div>
